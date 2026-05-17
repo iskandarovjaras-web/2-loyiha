1b308de34387b3b1f60a4313b942c04916e98ac1
iskandarovjaras-web <iskandarovjaras@gmail.com>
2026-05-17 21:21:13 +0500

    new folder

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var buyBtn = document.querySelector(".buy-btn");
+var cartBtn = document.querySelector(".cart-btn");
+var minus = document.querySelector(".minus");
+var soni = document.querySelector(".soni");
+var plus = document.querySelector(".plus");
+
+
+plus.addEventListener("click",  function() {
+    soni.textContent++
+});
+minus.addEventListener("click",  function() {
+    soni.textContent--
+});
